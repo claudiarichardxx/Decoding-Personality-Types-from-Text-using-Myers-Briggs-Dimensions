963a39b65224abeecaa28f7ca227658f79a9a590
claudiarichardxx <claudiarichardxx@gmail.com>
2024-02-25 19:25:41 -0500

    bugs

--- pipeline.py
+++ pipeline.py
@@ -14,17 +14,17 @@ class Pipeline:
         visualize = Visualize()
         model_name = 'ClaudiaRichard/mbti-bert-nli-finetuned'
         pred.load_model(model_name)
-        self.encoded, self.labels = pred.tokenizer(text, return_attention_mask=True, return_tensors="pt"), pred.predict_labels(text)
+        self.encoded, labels = pred.tokenizer(text, return_attention_mask=True, return_tensors="pt"), pred.predict_labels(text)
         htmls = []
         attribute.initialize(pred.model)
         tokens = pred.tokenizer.convert_ids_to_tokens(self.encoded["input_ids"][0])
         for i in range(0,4):
             attributions = attribute.getAttributions(self.encoded, i)
-            print(self.labels)
-            self.labels = list(self.labels)
-            if(self.checkThreshold(self, attributions, label = int(self.labels[i]))):
+            #print(self.labels)
+            #self.labels = list(self.labels)
+            if(self.checkThreshold(self, attributions, label = int(labels[i]))):
 
-                html = visualize.attributions_to_html(tokens, attributions.numpy()[0], label = int(self.labels[i]))
+                html = visualize.attributions_to_html(tokens, attributions.numpy()[0], label = int(labels[i]))
                 htmls.append('Label: '+ labs[i][int(self.labels[i])])
                 label_interpret = f""" <span style="background-color: {Color('cyan').hex}">{labs[i][0]}</span> <span style="background-color: {Color('violet').hex}">{labs[i][1]}</span>"""
                 htmls.append('<br>' + label_interpret+ '<br>')
